68b020127ee5a2680db51117abd925a06afb8c1e
Yam <yam@debian>
2013-09-21 02:43:37 +0300

    Slight touch-ups

--- server.cc
+++ server.cc
@@ -7,17 +7,19 @@ std::string ProgramName;
 
 void Die(std::string msg)
 {
-	std::cerr << ProgramName <<": error: " << msg << std::endl;
+	std::cerr << ProgramName << ": error: " << msg << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-	ProgramName = basename(argv[0]);
+	ProgramName = ::basename(argv[0]);
 
-	int sockFd = socket(AF_INET, SOCK_STREAM, 0);
+	int sockFd = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (sockFd == -1)
 		Die("Failed to open socket");
 
+
+	
 
   return 0;
 }
