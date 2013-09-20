7013cd9a785d58f85516812f938cbed9feb0e185
Yam <yam@debian>
2013-09-21 02:45:53 +0300

    Changed my mind about global qualifications

--- server.cc
+++ server.cc
@@ -14,9 +14,9 @@ void Die(std::string msg)
 
 int main(int argc, char** argv)
 {
-	ProgramName = ::basename(argv[0]);
+	ProgramName = basename(argv[0]);
 
-	int sockFd = ::socket(AF_INET, SOCK_STREAM, 0);
+	int sockFd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockFd == -1)
 		Die("Failed to open socket");
 
