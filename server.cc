eab68df763b7458d911844ab11d6fed1ea76af6f
Yam <yam@debian>
2013-09-21 02:38:46 +0300

    Add some infrastructure, and opena socket

--- server.cc
+++ server.cc
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <libgen.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
-int main()
+std::string ProgramName;
+
+void Die(std::string msg)
 {
-  std::cout << "Hello world!" << std::endl;
+	std::cerr << ProgramName <<": error: " << msg << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+	ProgramName = basename(argv[0]);
+
+	int sockFd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockFd == -1)
+		Die("Failed to open socket");
+
 
   return 0;
 }
