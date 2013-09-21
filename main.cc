b0fab5ee0828cf92ab65771dda54d4c20a58fb8e
Yam <yam@debian>
2013-09-22 01:54:05 +0300

    No need for program name anymore

--- main.cc
+++ main.cc
@@ -1,14 +1,9 @@
 #include "tcp_listener.h"
 
 #include <iostream>
-#include <libgen.h>
-
-std::string ProgramName;
 
 int main(int argc, char** argv)
 {
-  ProgramName = basename(argv[0]);
-
   ymarcov::net::TcpListener listener(1334);
   listener.Listen();
 
