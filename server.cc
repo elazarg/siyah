dd3fb21ff96dc697f79aec7ad81162416355192e
Yam <yam@debian>
2013-10-07 15:58:20 +0300

    Implemented dumb server that only accepts a client and does nothing

--- server.cc
+++ server.cc
@@ -1,27 +1,87 @@
+#include <boost/lexical_cast.hpp>
+#include <stdexcept>
 #include <iostream>
 #include <libgen.h>
-#include <sys/types.h>
+#include <memory>
+#include <netdb.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 std::string ProgramName;
 
-void Die(std::string msg)
+class TcpListener
 {
-	std::cerr << ProgramName << ": error: " << msg << std::endl;
-	_exit(1);
-}
+	public:
+		TcpListener(int port)
+			: _port(port)
+		{
+			auto hints = GetHints();
+
+			_fd = socket(hints.ai_family, hints.ai_socktype, hints.ai_protocol);
+
+			if (_fd == -1)
+				throw std::runtime_error("Failed to create socket");
+
+			Bind();
+		}
+
+		void Listen()
+		{
+			if (listen(_fd, SOMAXCONN))
+				throw std::runtime_error("Failed to start listening");
+		}
+
+		int AcceptClient()
+		{
+			int fd = accept(_fd, nullptr, nullptr);
+
+			if (fd == -1)
+				throw std::runtime_error("Failed to accept client socket");
+
+			return fd;
+		}
+
+	private:
+		struct addrinfo GetHints() const
+		{
+			struct addrinfo hints = {0};
+
+			hints.ai_family = AF_INET;
+			hints.ai_socktype = SOCK_STREAM;
+			hints.ai_flags = AI_PASSIVE;
+
+			return hints;
+		}
+
+		void Bind()
+		{
+			std::unique_ptr<struct addrinfo> address;
+			struct addrinfo* result;
+			auto hints = GetHints();
+
+			if (getaddrinfo("localhost", boost::lexical_cast<std::string>(_port).c_str(), &hints, &result))
+				throw std::runtime_error("Failed to resolve host");
+
+			address.reset(result);
+
+			if (bind(_fd, address->ai_addr, address->ai_addrlen))
+				throw std::runtime_error("Failed to bind socket to the specified port");
+		}
+
+		int _port;
+		int _fd;
+};
 
 int main(int argc, char** argv)
 {
 	ProgramName = basename(argv[0]);
 
-	int sockFd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockFd == -1)
-		Die("Failed to open socket");
+	TcpListener listener(1334);
+	listener.Listen();
 
-
-	
+	int client = listener.AcceptClient();
+	std::cout << "Client connected" << std::endl;
 
   return 0;
 }
